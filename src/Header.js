d7d190021afef9cdc82c52932075220ffc59beb3
Wade Stewart <stew.wade@gmail.com>
2018-07-08 15:11:31 -0400

    added header

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="App-header">
+                <h1 className="App-title">React Counters</h1>
+                <button>+</button>
+                <button>-</button>
+            </header>
+        )
+    }
+}
